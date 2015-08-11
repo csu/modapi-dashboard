3886062eb0f132890aa8b34e5806225c4bd268eb
Christopher Su <christophersu9@gmail.com>
2015-08-10 22:35:48 -0700

    functiona

--- routes.py
+++ routes.py
@@ -1,3 +1,6 @@
+import datetime
+from dateutil import parser
+
 from flask import Blueprint, request, render_template
 import requests
 
@@ -11,5 +14,24 @@ module = Blueprint(config['module_name'], __name__,
 @module.route('/')
 @require_secret
 def dashboard_index():
-    
+    grid_items = []
+
+    # GitHub module
+    github_item = {}
+    github_item['title'] = 'GitHub Commit'
+    is_complete = requests.get(secrets.modapi_url('/github/streak/?onlyNotifyWhenIncomplete=true')).json()
+    is_complete = is_complete['is_complete']
+    github_item['body'] = 'Complete' if is_complete else 'Incomplete'
+    grid_items.append(github_item)
+
+    # Countdowns
+    today = datetime.date.today()
+    for item in secrets.COUNTDOWN_ITEMS:
+        then = parser.parse(item['date']).date()
+        diff = then - today
+        grid_items.append({
+            'title': item['title'],
+            'body': '%s days' % diff.days
+        })
+
     return render_template('dashboard.html', grid_items=grid_items)
