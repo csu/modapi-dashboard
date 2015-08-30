1ae035e65c524c90df1eda8857bee621b14e12ee
Christopher Su <christophersu9@gmail.com>
2015-08-30 16:57:51 -0700

    todoist

--- routes.py
+++ routes.py
@@ -13,11 +13,9 @@ module = Blueprint(config['module_name'], __name__,
                     template_folder='templates',
                     static_folder='static')
 
-def get_dashboard_item_routes():
-    routes = secrets.DASHBOARD_ITEM_ROUTES[:]
-
-    todoist_tasks_route = secrets.TODOIST_BASE_URL
-    for task in secrets.TODOIST_TASKS:
+def get_todoist_route(base, list):
+    todoist_tasks_route = base
+    for task in list:
         task_text = task['task'].replace(' ', '%20')
         if '?' in todoist_tasks_route:
             todoist_tasks_route += '&query=%s' % task_text
@@ -27,8 +25,13 @@ def get_dashboard_item_routes():
         if 'title' in task:
             task_title = task['title'].replace(' ', '%20')
             todoist_tasks_route += '--%s' % task_title
-    routes.append(todoist_tasks_route)
+    return todoist_tasks_route
 
+
+def get_dashboard_item_routes():
+    routes = secrets.DASHBOARD_ITEM_ROUTES[:]
+    routes.append(get_todoist_route(secrets.TODOIST_TASKS_BASE_URL, secrets.TODOIST_TASKS))
+    routes.append(get_todoist_route(secrets.TODOIST_SINCE_BASE_URL, secrets.TODOIST_SINCE))
     return routes
 
 @module.route('/')
