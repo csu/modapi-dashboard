5012728b7e807d796672dde8515537a6f519729a
Christopher Su <christophersu9@gmail.com>
2015-08-31 21:48:47 -0700

    routes

--- routes.py
+++ routes.py
@@ -13,32 +13,11 @@ module = Blueprint(config['module_name'], __name__,
                     template_folder='templates',
                     static_folder='static')
 
-def get_todoist_route(base, list):
-    todoist_tasks_route = base
-    for task in list:
-        task_text = task['task'].replace(' ', '%20')
-        if '?' in todoist_tasks_route:
-            todoist_tasks_route += '&query=%s' % task_text
-        else:
-            todoist_tasks_route += '?query=%s' % task_text
-
-        if 'title' in task:
-            task_title = task['title'].replace(' ', '%20')
-            todoist_tasks_route += '--%s' % task_title
-    return todoist_tasks_route
-
-
-def get_dashboard_item_routes():
-    routes = secrets.DASHBOARD_ITEM_ROUTES[:]
-    routes.append(get_todoist_route(secrets.TODOIST_TASKS_BASE_URL, secrets.TODOIST_TASKS))
-    routes.append(get_todoist_route(secrets.TODOIST_SINCE_BASE_URL, secrets.TODOIST_SINCE))
-    return routes
-
 @module.route('/')
 @require_secret
 def dashboard_index():
     return render_template('dashboard.html', secret_key=secret_key,
-            routes=get_dashboard_item_routes())
+            routes=secrets.dashboard_routes())
 
 @module.route('/countdowns')
 @module.route('/countdowns/')
